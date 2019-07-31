f3c81360f35a581436c5b6088fa4ecec2aad1f0b
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-31 17:38:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,9 +10,39 @@ const Home = () => {
   );
 };
 
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+ 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
 ReactDOM.render((
   <Router>
-    <Route path="/" render={Home} />
+    <React.Fragment>
+      <Route path="/" render={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+    </React.Fragment>
   </Router>),
   document.getElementById('root')
 );
